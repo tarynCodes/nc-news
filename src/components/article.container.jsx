1d1e6960a31adce23cbac43220b82318c719bfc0
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-09-05 14:01:51 +0100

    added loading pattern for article container

--- src/components/article.container.jsx
+++ src/components/article.container.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticleCard from './article-card'
 import getArticles from '../Api';
 
 
 const ArticleContainer = ({ articles, setArticles }) => {
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     getArticles().then((articles) => {
       setArticles(articles);
+      setIsLoading(false)
     });
   }, []);
+
+  if (isLoading) return <p>Loading...</p>
 
   return (
     <div>
