13aacd88badde659332cc3a1cf56f5f9f05844c3
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-09-05 11:14:27 +0100

    adding css for mobile

--- src/components/article-card.jsx
+++ src/components/article-card.jsx
@@ -1,18 +1,21 @@
- const ArticleCard = ({articles}) => {
-        return (
-          <ul>
-            {articles.map((article) => (
-              <li key={article.id} className="article-card">
-                <img src={article.article_img_url} alt={article.title} />
-                <div className="article-details">
-                 <h3>{article.title}</h3>
-                  <p>Author: {article.author}</p>
-                <p>Topic: {article.topic}</p>   
-               </div>
-              </li>
-            ))}
-          </ul>
-        );
-      };
+
+
+const ArticleCard = ({ articles }) => {
+  const articleList = articles.map((article) => (
+    <li key={article.id} className="article-card">
+      {/* <Link to={`/article/${article.article_id}`}> */}
+      <img src={article.article_img_url} alt={article.title} />
+      <div className="article-details">
+        <h3>{article.title}</h3>
+        <p>Author: {article.author}</p>
+        <p>Topic: {article.topic}</p>
+      </div>
+      {/* </Link> */}
+    </li>
+  ));
+
+  return <ul key={articleList} className="article-list">{articleList}</ul>;
+};
 
 export default ArticleCard; 
+
