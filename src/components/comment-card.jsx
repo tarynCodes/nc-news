c986c9ed1164506535611d028b407314e3effa07
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-09-06 10:23:03 +0100

    css applied and finished rendering certain details of comment

--- src/components/comment-card.jsx
+++ src/components/comment-card.jsx
@@ -4,6 +4,8 @@ const CommentCard = ({comments}) => {
         <li key={comment.id} className="comment-card">
             <h3>{comment.author}comment author</h3>
             <p>{comment.body}</p>
+            <h4>{comment.votes}</h4>
+            <h5>{comment.created_at}</h5>
         </li>
     ))
    
